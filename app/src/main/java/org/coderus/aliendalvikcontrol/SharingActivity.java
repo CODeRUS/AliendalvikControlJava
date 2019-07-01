a4076d107395b8ff702b5575c763ed92428a4cbb
Andrey Kozhevnikov <a.kozhevnikov@omprussia.ru>
2019-07-01 17:29:45 +0300

    Writing content file to temp location

--- app/src/main/java/org/coderus/aliendalvikcontrol/SharingActivity.java
+++ app/src/main/java/org/coderus/aliendalvikcontrol/SharingActivity.java
@@ -5,9 +5,15 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.MimeTypeMap;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class SharingActivity extends Activity {
     private static final String TAG = "SharingActivity";
@@ -27,12 +33,37 @@ public class SharingActivity extends Activity {
         Log.w(TAG, receivedIntent.toString());
 
         final Uri fileUrl = receivedIntent.getParcelableExtra(Intent.EXTRA_STREAM);
+        final String fileScheme = fileUrl.getScheme();
+        Log.w(TAG, fileScheme);
         String fileName = fileUrl == null ? new String() : fileUrl.toString();
-        String data = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
-        if (data == null) {
-            data = new String();
-        }
         final String mimeType = receivedIntent.getType();
+        String data = receivedIntent.getStringExtra(Intent.EXTRA_TEXT);
+        final String extension = MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType);
+        if (data == null) {
+            data = "temp-share";
+        }
+        if (fileScheme.equals("content")) {
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(fileUrl);
+
+                File file = new File(getFilesDir(), data + "." + extension);
+                OutputStream output = new FileOutputStream(file);
+
+                byte[] buffer = new byte[4 * 1024]; // or other buffer size
+                int read;
+
+                while ((read = inputStream.read(buffer)) != -1) {
+                    output.write(buffer, 0, read);
+                }
+
+                output.flush();
+                output.close();
+
+                fileName = file.getAbsolutePath();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
 
         JSONObject json = new JSONObject();
         try {
